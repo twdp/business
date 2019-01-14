88440017fda4080734af376a3951c1e0369795b9
李万绪 <twdotp@gmail.com>
2019-01-14 10:18:25 +0800

    add extra

--- model/base.go
+++ model/base.go
@@ -1,9 +1,46 @@
 package model
 
-import "github.com/astaxie/beego/orm"
+import (
+	"encoding/json"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/orm"
+)
 
 type Base struct {
 	Id        int64             `json:"id"`
 	CreatedAt orm.DateTimeField `json:"created_at" orm:"auto_now_add"`
 	UpdatedAt orm.DateTimeField `json:"updated_at" orm:"auto_now"`
+
+	Extra map[string]interface{} `orm:"-" json:"extra"`
+
+	ExtraJson string `orm:"type(text)" json:"-"`
+}
+
+
+func (b *Base) SetExtra(m map[string]interface{}) {
+	mm, err := json.Marshal(m)
+	if err != nil {
+		logs.Error("marshal extra failed. m: %v, err: %v", m, err)
+	}
+	b.ExtraJson = string(mm)
+}
+
+func (b *Base) GetExtra() map[string]interface{} {
+	if b.Extra == nil && b.ExtraJson != ""{
+		b.Extra = make(map[string]interface{})
+		err := json.Unmarshal([]byte(b.ExtraJson), b.Extra)
+		if err != nil {
+			logs.Error("unmarshal extra failed. extra-json: %v, err: %v", b.ExtraJson, err)
+		}
+		return b.Extra
+	} else if b.Extra == nil {
+		b.Extra = make(map[string]interface{})
+		return b.Extra
+	} else {
+		return b.Extra
+	}
+}
+
+func (b *Base) AddVariable(n string, p interface{}) {
+	b.GetExtra()[n] = p
 }
