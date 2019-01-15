d5f141cbda31b343168865b45cf371a712e017b5
李万绪 <twdotp@gmail.com>
2019-01-15 09:52:04 +0800

    bugfix

--- controller/base.go
+++ controller/base.go
@@ -7,32 +7,6 @@ import (
 	"tianwei.pro/business"
 )
 
-type TemplateController struct {
-	beego.Controller
-}
-
-func (t *TemplateController) E500(body string) {
-	t.Code(500, body)
-}
-
-func (t *TemplateController) Code(code int, body string) {
-	out := t.Ctx.Output
-	out.SetStatus(code)
-	out.Body([]byte(body))
-}
-
-func (t *TemplateController) Return(body string) {
-	t.Code(200, body)
-}
-
-func (r *TemplateController) ReadBody(result interface{}) {
-	b := r.Ctx.Input.RequestBody
-	err := json.Unmarshal(b, result)
-	if business.IsError(err) {
-		logs.Error("read body failed. %v", err)
-	}
-}
-
 type RestfulController struct {
 	beego.Controller
 }
@@ -49,4 +23,22 @@ func (r *RestfulController) ReadBody(result interface{}) {
 	if business.IsError(err) {
 		logs.Error("read body failed. %v", err)
 	}
+}
+
+func (r *RestfulController) E500(body string) {
+	r.Code(500, body)
+}
+
+func (r *RestfulController) Code(code int, body string) {
+	out := r.Ctx.Output
+	out.SetStatus(code)
+	out.Body([]byte(body))
+}
+
+func (r *RestfulController) Return(body interface{}) {
+	b, err := json.Marshal(body)
+	if business.IsError(err) {
+		logs.Error("read body failed. %v", err)
+	}
+	r.Code(200, string(b))
 }
